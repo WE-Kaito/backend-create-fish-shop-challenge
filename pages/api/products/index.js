b61c24fd4eb9ac9e9b8bddc32664be0bd3bfa330
Enrico Weißenrieder <e.weissenrieder94@gmail.com>
2023-03-27 11:53:22 +0200

    finished challenge

--- pages/api/products/index.js
+++ pages/api/products/index.js
@@ -8,4 +8,14 @@ export default async function handler(request, response) {
     const products = await Product.find();
     return response.status(200).json(products);
   }
+
+  if (request.method === "POST") {
+    try {
+      const product = new Product(request.body);
+      await product.save();
+      return response.status(201).json({ status: "ok" });
+    } catch (error) {
+      return response.status(500).json({ message: error.message });
+    }
+  }
 }
